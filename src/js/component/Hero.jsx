267b61db2662ec9ff4f4af364299cb5f239d6fc7
nacho131994 <nachommunoz13@gmail.com>
2023-03-17 19:20:10 +0000

    componente Hero

---
+++ src/js/component/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Hero =()=>{
+    return (
+        <div  id="divPrincipal" className="wrapper bg-dark text-white" >
+            <div className="title ">
+                <h1>Creador de Contraseñas</h1>
+            </div>
+            <div className="description">
+                <p>esto es una descripcion de la app</p>
+            </div>
+        </div>
+
+
+    )
+}
+export default Hero;
---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar =()=>{
+
+    return(<nav className="navbar navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand">Navbar</a>
+      <div className="d-flex">
+        <a href="" className="nav-link m-3">Home</a>
+        <a  href="" className="nav-link m-3">Contraseñas guardadas</a>
+    </div>
+    </div>
+    </nav>)
+    }
+    
+    export default Navbar;
+    
