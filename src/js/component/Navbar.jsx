6e62af12a5be85a975cf5c6751f41ce016e92d4d
meryvl <meryvalerolaguna@gmail.com>
2023-03-17 19:00:00 +0000

    commit

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar =()=>{
+
+return(<nav class="navbar navbar-light bg-light">
+<div class="container-fluid">
+  <a class="navbar-brand">Navbar</a>
+  <div class="d-flex">
+    <a href="" className="nav-item m-3">Home</a>
+    <a  href="" className="m-3">Contraseñas guardadas</a>
+</div>
+</div>
+</nav>)
+}
+
+export default Navbar;
